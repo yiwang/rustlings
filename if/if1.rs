aadb6ec8bba0c3625f11aca7230e4c5b459abc5a
Yi Wang <e@yi-wang.me>
2016-08-15 23:23:21 -0400

    done strings

--- if/if1.rs
+++ if/if1.rs
@@ -1,4 +1,9 @@
-pub fn bigger(a: i32, b:i32) -> i32 {
+pub fn bigger(a: i32, b: i32) -> i32 {
+    if a > b {
+        a
+    } else {
+        b
+    }
     // Complete this function to return the bigger number!
     // Do not use:
     // - return
--- strings/strings1.rs
+++ strings/strings1.rs
@@ -6,7 +6,8 @@ fn main() {
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    // "blue".to_string()
+    String::from("blue")
 }
 
 
